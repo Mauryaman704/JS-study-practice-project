b625f9229d238e46063b1bea0c8e8a14d08be96a
Mauryaman704 <am9198566563@gmail.com>
2025-10-14 19:57:22 +0530

    study

--- digitalClock.js
+++
@@ -1,7 +0,0 @@
-const clock = document.getElementById('clock');
-// document.querySelector('clock')
-
-setInterval(function () {
-  let date = new Date();
-  // console.log(date.toLocaleTimeString());
-}, 1000);
